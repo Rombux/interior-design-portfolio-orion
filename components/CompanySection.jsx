0ede79fe5176732b347bf274d838178fb013bd1c
Ahmad Kahla <ahmad.w.kahla@gmail.com>
2024-05-07 14:53:40 +0300

    Company section update

--- components/CompanySection.jsx
+++ components/CompanySection.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function CompanySection() {
     return (
-        <div className="py-4 bg-zinc-100">
+        <div className="my-4 py-4 bg-zinc-100">
             <p className="py-4 text-lg font-medium text-center opacity-40">Trusted by the world’s best companies</p>
             <div className="container flex flex-wrap items-center justify-between">
 
